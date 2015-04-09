070a164b7d663d58e5a1a87bee7d1307a727704f
Olivier Brunel <jjk@jjacky.com>
2015-04-09 20:20:06 +0200

    Add GPL headers

--- src/libanopa/service_internal.h
+++ src/libanopa/service_internal.h
@@ -1,3 +1,24 @@
+/*
+ * anopa - Copyright (C) 2015 Olivier Brunel
+ *
+ * service_internal.h
+ * Copyright (C) 2015 Olivier Brunel <jjk@jjacky.com>
+ *
+ * This file is part of anopa.
+ *
+ * anopa is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * anopa is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * anopa. If not, see http://www.gnu.org/licenses/
+ */
 
 #ifndef AA_SERVICE_INTERNAL_H
 #define AA_SERVICE_INTERNAL_H
